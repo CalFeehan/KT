65f6e80796afef97a3cdd00eafb9c03dc2730fa1
Callum Feehan <feehan07@hotmail.co.uk>
2024-04-29 20:21:15 +0100

    added unit test command handler

---
+++ KT.Application.Tests/Students/Commands/CreateCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using KT.Application.Common.Interfaces.Persistence;
+using KT.Application.Students.Commands;
+using Moq;
+using FluentAssertions;
+using KT.Domain.StudentAggregate;
+
+namespace KT.Application.Tests.Students.Commands;
+
+public class CreateCommandHandlerTests
+{
+    private readonly Mock<IStudentRepository> _studentRepositoryMock;
+    private readonly CreateCommandHandler _createCommandHandler;
+
+    public CreateCommandHandlerTests()
+    {
+        _studentRepositoryMock = new Mock<IStudentRepository>();
+        _createCommandHandler = new CreateCommandHandler(_studentRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task Handle_WhenStudentIsCreated_ShouldReturnStudent()
+    {
+        // Arrange
+        var command = new CreateCommand(
+            "John",
+            "Doe",
+            DateOnly.FromDateTime(new DateTime(2000, 1, 1)));
+
+        var student = Student.Create(command.Forename, command.Surname, command.DateOfBirth);
+
+        _studentRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<Student>()))
+            .ReturnsAsync(student);
+
+        // Act
+        var result = await _createCommandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Value.Should().Be(student);
+    }
+}
--- KT.Application/Students/Commands/CreateCommandHandler.cs
+++ KT.Application/Students/Commands/CreateCommandHandler.cs
@@ -5,14 +5,20 @@ using MediatR;
 
 namespace KT.Application.Students.Commands;
 
-public class CreateCommandHandler(IStudentRepository studentRepository)
-    : IRequestHandler<CreateCommand, ErrorOr<Student>>
+public class CreateCommandHandler : IRequestHandler<CreateCommand, ErrorOr<Student>>
 {
+    private readonly IStudentRepository _studentRepository;
+
+    public CreateCommandHandler(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
     public async Task<ErrorOr<Student>> Handle(CreateCommand command, CancellationToken cancellationToken)
     {
         var student = Student.Create(command.Forename, command.Surname, command.DateOfBirth);
 
-        var created = await studentRepository.CreateAsync(student);
+        var created = await _studentRepository.CreateAsync(student);
 
         return created;
     }
