3a418940ee32b167fb23eecc3a109e5fdb9d1c0a
Callum Feehan <feehan07@hotmail.co.uk>
2024-05-18 22:05:04 +0100

    CTMT Config

---
+++ Infrastructure/KT.Infrastructure/Persistence/Configurations/CourseTemplateModuleTemplateConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using KT.Domain.CourseTemplateAggregate.Entities;
+using KT.Domain.CourseTemplateAggregate.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KT.Infrastructure.Persistence.Configurations;
+
+public class CourseTemplateModuleTemplateConfiguration : IEntityTypeConfiguration<CourseTemplateModuleTemplate>
+{
+    public void Configure(EntityTypeBuilder<CourseTemplateModuleTemplate> builder)
+    {
+        builder.ToTable("CourseTemplateModuleTemplates", "CourseTemplate");
+
+        builder.HasKey(ctmt => ctmt.Id);
+        builder.Property(ctmt => ctmt.Id).ValueGeneratedNever();
+
+        builder.Property(ctmt => ctmt.CourseTemplateId)
+            .IsRequired();
+
+        builder.Property(ctmt => ctmt.ModuleTemplateId)
+            .IsRequired();
+    }
+}
